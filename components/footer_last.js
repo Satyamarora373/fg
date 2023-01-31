a41d1cc6988a4b5c351f736a1023470ad0487e89
Satyam Arora <64372833+Satyamarora373@users.noreply.github.com>
2023-01-31 14:44:13 +0530

    footer js

--- components/footer_last.js
+++ components/footer_last.js
@@ -9,70 +9,67 @@ import Location from "./location"
 export const Last_element = () => {
   const router = useRouter();
   return (
-    <div className={styles.main}>
-      <br />
-      <div className={styles.flexContaine}>
+<div className={styles.ppp}>
+      <div className={styles.footer}>
+        <div class={styles.container}>
+          <div class={styles.secabout}>
+            <Logo />
+            <h2>Fintract Global</h2>
+            <p style={{color:"white"}}>
+            At Fintract Global Ltd, we combine a cutting-edge tech stack with exceptional talent from Europe, Asia and America to lead change in how financial entities work. Headquartered in London, Fintract Global develops cutting edge fintech and regtech products.
+            </p>
+            <ul class={styles.some}>
+              <li>
+                <SocialIcon network="instagram" bgColor="#fff" style={{ height: "2.5rem", width: "2.5rem",marginRight:".6em" }}></SocialIcon>
+              </li>
+              <li>
+              <SocialIcon network="linkedin" bgColor="#fff" style={{ height: "2.5rem", width: "2.5rem",marginRight:".6em" }}></SocialIcon>
+              </li>
+              <li>
+              <SocialIcon network="twitter" bgColor="#fff" style={{ height: "2.5rem", width: "2.5rem",marginRight:".6em" }}></SocialIcon>
+              </li>
+              <li>
+              <SocialIcon network="facebook" bgColor="#fff" style={{ height: "2.5rem", width: "2.5rem",marginRight:".6em" }}></SocialIcon>
+              </li>
+            </ul>
+          </div>
 
-  <div className={styles.flexchild }>
- 
-    <Logo />
-    <p style={{color:"white",fontStyle:"Roboto",fontSize:"3.24vw",paddingLeft:"2vw"}}>Fintract Global</p>
-    <p style={{color:"white",paddingLeft:"2vw",width:"52vw"}}>At Fintract Global Ltd, we combine a cutting-edge tech stack with exceptional talent from Europe, Asia and America to lead change in how financial entities work. Headquartered in London, Fintract Global develops cutting edge fintech and regtech products.</p>
-    <div style={{paddingLeft:"2vw",display:"inline-block",marginTop:"2em"}}>
-    <SocialIcon network="twitter" bgColor="#fff" style={{ height: "2.5vw", width: "2.5vw",marginRight:".6em" }}><p style={{color:"white"}}></p></SocialIcon>
-    <SocialIcon network="instagram" bgColor="#fff" style={{ height: "2.5vw", width: "2.5vw",marginRight:".6em" }}></SocialIcon>
-    <SocialIcon network="facebook"bgColor="#fff" style={{ height: "2.5vw", width: "2.5vw",marginRight:".6em" }}></SocialIcon>
-    <SocialIcon network="linkedin" bgColor="#fff" style={{ height: "2.5vw", width: "2.5vw",marginRight:".6em" }}></SocialIcon>
-    </div>/''''''''''''
-    
-  </div>
-  
-  <div className={styles.flexchild }>
-    <div style={{display:"flex",flexWrap:"nowrap"}}>
-          <div>
-          <p style={{color:"#fff",marginLeft:"1.5vw",marginLeft:"1.5vw"}}>Address</p>
-            <br />
-            <p style={{color:"#fff",marginLeft:"1.5vw",fontSize:"13px"}}>Registered Office: London Office Address 71-75, Sheldon Street, Covent Garden London, WC2h 9JQ</p>
-            
+          <div class="sec contact">
+            {/* <h2>Contact Info</h2> */}
+            <ul className={styles.info}>
+              <li>
+                <h2>Address <br /></h2>
+                <span style={{color:"white"}}>Registered Office: London<br /> Office Address 71-75, <br />Sheldon Street, Covent Garden<br /> London, WC2h 9JQ</span>
+                
+              </li>
+              <br/><br />
+              <li>
+                <h3 style={{color:"white"}}>Phone</h3>
+                <p style ={{color:"white"}}><a href="tel:442037373227">+44-2037373227</a></p>
+              </li>
+              <br/>
+              <li>
+                <h3 style={{color:"white"}}>Email</h3>
+                <p style={{color:"white"}}>
+                  <a href="malito:contact@fintract.co.uk">contact@fintract.co.uk</a>
+                </p>
+              </li>
+            </ul>
           </div>
-     
-    </div>
-    <br />
-    <br />
-    <div style={{display:"flex",flexWrap:"nowrap"}}>
           <div>
-          <p style={{color:"#fff",marginLeft:"1.5vw",marginLeft:"1.5vw"}}>Phone</p>
-            <br />
-            <p style={{color:"#fff",marginLeft:"1.5vw",fontSize:"13px"}}>+44-2037373227</p>
-            
-          </div>
-          
-     
-    </div>
-    <br />
-    <br />
-    <div style={{display:"flex",flexWrap:"nowrap"}}>
-          <div>
-          <p style={{color:"#fff",marginLeft:"1.5vw",marginLeft:"1.5vw"}}>Email</p>
-            <br />
-            <p style={{color:"#fff",marginLeft:"1.5vw",fontSize:"13px"}}>contact@fintract.co.uk</p>
-            
-          </div>
-          
-     
+            <br /><br /><br />
+        <p style={{alignContent:"center"}}>Fintract Global takes your privacy very seriously. We may process your personal information for carefully considered and specific purposes which are in our interests and enable us to enhance the services we provide, but which we believe also benefit our customers. View our Privacy Policy to learn more about these interests and when we may process your information in this way.</p>
+      </div>
+      
+        </div>
+      </div>
+      
+      <div >
+        <p style={{backgroundColor:"black",color:"white"}}>
+          Copyright © 2021. All rights reserved
+        </p>
+      </div>
     </div>
 
-  </div>
-  
-</div>
-<div >
-  <p style={{color:"white",width:" 83vw",textAlign:"center",size:"23px"}}> Fintract Global takes your privacy very seriously. We may process your personal information for carefully considered and specific purposes which are in our interests and enable us to enhance the services we provide, but which we believe also benefit our customers. View our Privacy Policy to learn more about these interests and when we may process your information in this way.</p>
-    <br></br>
-    <p style={{textAlign:"center"}}><a style={{color:"white",textAlign:"center"}}><u>Click here</u> </a></p>
-    {/* <br /> */}
-    <p style={{textAlign:"center",color:"#9B9B9B"}}> Copyright ©2021 All rights reserved.</p>
-</div>  
-      <div style={{marginTop: "2vw"}}></div>
-    </div>
   );
 };
